0abb4182c5479d9b254e961207624701e87f791e
Jason <jason.lo.kc@gmail.com>
2020-09-21 02:59:52 -0700

    deployed on github pages

---
+++ docs/precache-manifest.961fcf8001d208d5a6715901f2ae1cec.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c580253f25960e5ddcece1bf646c0589",
+    "url": "/omdb-challenge/index.html"
+  },
+  {
+    "revision": "326f698a97cff34fd73d",
+    "url": "/omdb-challenge/static/css/main.8b3b7a51.chunk.css"
+  },
+  {
+    "revision": "4b03a069162c774b482a",
+    "url": "/omdb-challenge/static/js/2.3a5c4e1f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/omdb-challenge/static/js/2.3a5c4e1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "326f698a97cff34fd73d",
+    "url": "/omdb-challenge/static/js/main.41f3665e.chunk.js"
+  },
+  {
+    "revision": "364e01e2acb49523e570",
+    "url": "/omdb-challenge/static/js/runtime-main.a69f2e81.js"
+  },
+  {
+    "revision": "0cb5a5c0d251c109458c85c6afeffbaa",
+    "url": "/omdb-challenge/static/media/fa-brands-400.0cb5a5c0.svg"
+  },
+  {
+    "revision": "13685372945d816a2b474fc082fd9aaa",
+    "url": "/omdb-challenge/static/media/fa-brands-400.13685372.ttf"
+  },
+  {
+    "revision": "a06da7f0950f9dd366fc9db9d56d618a",
+    "url": "/omdb-challenge/static/media/fa-brands-400.a06da7f0.woff2"
+  },
+  {
+    "revision": "c1868c9545d2de1cf8488f1dadd8c9d0",
+    "url": "/omdb-challenge/static/media/fa-brands-400.c1868c95.eot"
+  },
+  {
+    "revision": "ec3cfddedb8bebd2d7a3fdf511f7c1cc",
+    "url": "/omdb-challenge/static/media/fa-brands-400.ec3cfdde.woff"
+  },
+  {
+    "revision": "261d666b0147c6c5cda07265f98b8f8c",
+    "url": "/omdb-challenge/static/media/fa-regular-400.261d666b.eot"
+  },
+  {
+    "revision": "89ffa3aba80d30ee0a9371b25c968bbb",
+    "url": "/omdb-challenge/static/media/fa-regular-400.89ffa3ab.svg"
+  },
+  {
+    "revision": "c20b5b7362d8d7bb7eddf94344ace33e",
+    "url": "/omdb-challenge/static/media/fa-regular-400.c20b5b73.woff2"
+  },
+  {
+    "revision": "db78b9359171f24936b16d84f63af378",
+    "url": "/omdb-challenge/static/media/fa-regular-400.db78b935.ttf"
+  },
+  {
+    "revision": "f89ea91ecd1ca2db7e09baa2c4b156d1",
+    "url": "/omdb-challenge/static/media/fa-regular-400.f89ea91e.woff"
+  },
+  {
+    "revision": "1ab236ed440ee51810c56bd16628aef0",
+    "url": "/omdb-challenge/static/media/fa-solid-900.1ab236ed.ttf"
+  },
+  {
+    "revision": "a0369ea57eb6d3843d6474c035111f29",
+    "url": "/omdb-challenge/static/media/fa-solid-900.a0369ea5.eot"
+  },
+  {
+    "revision": "b15db15f746f29ffa02638cb455b8ec0",
+    "url": "/omdb-challenge/static/media/fa-solid-900.b15db15f.woff2"
+  },
+  {
+    "revision": "bea989e82b07e9687c26fc58a4805021",
+    "url": "/omdb-challenge/static/media/fa-solid-900.bea989e8.woff"
+  },
+  {
+    "revision": "ec763292e583294612f124c0b0def500",
+    "url": "/omdb-challenge/static/media/fa-solid-900.ec763292.svg"
+  }
+]);
